9636b388ad6119737c44a8f8b51fadacaff6037b
DKZ <davidkingzyb@163.com>
2016-05-04 21:06:00 +0800

    tree init

---
+++ clio/tree.py
@@ -0,0 +1,77 @@
+# coding: utf-8
+"""
+
+license:MIT
+
+Copyright (c) 2016 DKZ
+
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software 
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+==============================================================================
+ ________  __         ______                                                  
+|   _____||  |       |_    _|                   _                       _     
+|  |      |  |         |  |     _____   __  __ | \_    _______  __  __ | \_   
+|  |      |  |         |  |    /     \ |  | | ||   _| |   __  ||  | | ||   _| 
+|  |_____ |  |_____   _|  |_  |   o   ||  |_| ||  |___|    ___||  |_| ||  |___
+|________||________| |______|  \_____/ |______|\_____/|___|    |______|\_____/
+==============================================================================
+2016/05/04 by DKZ https://davidkingzyb.github.io
+
+"""
+
+import json
+import re
+
+
+if __name__ == '__main__':
+    j={'root':[
+        {'ao1':[
+            'bv',
+            {'boo':'o1'},
+            {'boo2':[
+                'boov','boo',1
+            ]}
+        ]},
+        'av',
+        1,
+        {'ao2':{
+            'a2a':'aa',
+            'a2b':{
+                'cc':[1,2,3],
+                'ccc':'cccv',
+                'cccc':4
+            }
+        }}
+    ]}
+    output=json.dumps(j,sort_keys=True,indent=6,separators=('',':'))
+    print(output)
+    # output=re.sub('\n +?\[\n','\n',output)
+    # output=re.sub('\n +?\{\n','\n',output)
+    output=re.sub('\n +?\}\n','\n',output)
+    output=re.sub('\n +?\]\n','\n',output)
+    output=re.sub('\[','',output)
+    # output=re.sub('\{','',output)
+    # output=re.sub('\}','',output)
+    output=re.sub('\]','',output)
+    def addleaf(matched):
+        m=matched.group(0)
+        return '|---'+m[-1:]
+    output=re.sub('    \S',addleaf,output)
+
+
+    print(output)
